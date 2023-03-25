f27798d5d6548af16c4c665cdf0f258015634b77
NikSkor <mr-zerx@yandex.ru>
2023-03-25 19:01:11 +0500

    update

---
+++ learnjs-tasks/Ч.1 Язык JavaScript/10/10.2 Пользовательские ошибки, расширение Error/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+class FormatError extends SyntaxError {
+  constructor(message) {
+    super(message);
+    this.name = 'FormatError';
+  }
+}
+
+let err = new FormatError("ошибка форматирования");
+
+alert( err.message );
+alert( err.name );
+alert( err.stack );
+
+alert( err instanceof FormatError );
+alert( err instanceof SyntaxError );
